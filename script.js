fb5e5fdbff1b8fcff7e2001a501c4601c6f28b6e
Shubham <shubhamkumarverma091@gmail.com>
2026-06-16 16:26:24 +0530

    adding hamburger

--- script.js
+++ script.js
@@ -33,3 +33,14 @@
   }
 
   typeRole();
+   function toggleMenu() {
+    const nav = document.getElementById('navLinks');
+    const btn = document.getElementById('hamburger');
+    nav.classList.toggle('open');
+    btn.classList.toggle('open');
+  }
+ 
+  function closeMenu() {
+    document.getElementById('navLinks').classList.remove('open');
+    document.getElementById('hamburger').classList.remove('open');
+  }
